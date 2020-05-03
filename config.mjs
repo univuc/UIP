5782354cbc92bf3e63731eec47c40b32da255659
potados99 <potados99@gmail.com>
2020-05-04 06:06:34 +0900

    CRU tested.

--- config.mjs
+++ config.mjs
@@ -1,14 +1,14 @@
 /**
- * This file is part of Univuc/{appname}.
+ * This file is part of Univuc/UIP.
  *
  * Copyright (C) 2020 Univuc <potados99@gmail.com>
  *
- * Univuc/{appname}is free software: you can redistribute it and/or modify
+ * Univuc/UIP is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Univuc/{appname} is distributed in the hope that it will be useful,
+ * Univuc/UIP is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
@@ -17,15 +17,15 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-import getEnv from './lib/common/utils/env';
+import getEnv from 'iab/lib/utils/env';
 
 export default {
-    port: getEnv('{appname}_PORT') || 21100,
+    server: {
+        port: getEnv('UIP_PORT'),
+    },
 
-    slack_token: getEnv('{appname}_SLACK_TOKEN') || 'token',
-    slack_signing_secret: getEnv('{appname}_SLACK_SIGNING_SECRET') || 'secret',
-
-    web_id: getEnv('{appname}_WEB_ID') || 'master',
-    web_pw: getEnv('{appname}_WEB_PW') || '1234',
+    db: {
+        uri: 'mongodb://localhost/uip',
+    },
 };
 
